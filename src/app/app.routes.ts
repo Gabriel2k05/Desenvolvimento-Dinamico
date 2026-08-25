ffab732e845ae4abef93d3fbd7447c0162d835bd
GABRIEL MARQUES ROSARIO DOS SANTOS <10710840543@unijorge.com.br>
2026-08-25 20:59:03 -0300

    Criacao do componente home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { Pessoa } from './pessoa/pessoa';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'pessoa', component: Pessoa }, { path: 'home', component: Home }];
