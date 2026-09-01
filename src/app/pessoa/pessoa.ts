8b698b6dc3679e3bf90401896ecad269c7e0f7de
Ednei <edneijr5@outlook.com>
2026-09-01 15:41:03 -0300

    Aula 3 : Modelo pessoa e listagem

--- src/app/pessoa/pessoa.ts
+++ src/app/pessoa/pessoa.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pessoa } from './pessoa.model';
 
 @Component({
   selector: 'app-pessoa',
@@ -6,8 +7,46 @@ import { Component } from '@angular/core';
   templateUrl: './pessoa.html',
   styleUrl: './pessoa.css',
 })
-export class Pessoa {
-  nome = "Gabriel";
-  curso = "Análise e Desenvolvimento de Sistemas";
-  email = "gabrielmarx04@gmail.com";
+export class Pessoas {
+
+  pessoas: Pessoa[] = [
+    {
+      id: 1,
+      nome: "Gabriel",
+      curso: "Análise e Desenvolvimento de Sistemas",
+      email: "gabrielmarx04@gmail.com",
+      ativo: true,
+      vinculo: "aluno",
+      telefone: "71999998888"
+
+    },
+    {
+      id: 2,
+      nome: "Ana",
+      curso: "Análise e Desenvolvimento de Sistemas",
+      email: "ana@gmail.com",
+      ativo: true,
+      vinculo: "aluno",
+      telefone: "71999998888"
+    },
+
+    {
+      id: 3,
+      nome: "Ednei",
+      curso: "Análise e Desenvolvimento de Sistemas",
+      email: "edn@gmail.com",
+      ativo: true,
+      vinculo: "aluno",
+      telefone: "71999998888"
+    },
+
+    {
+      id: 4,
+      nome: "Cleberson",
+      curso: "Análise e Desenvolvimento de Sistemas",
+      email: "cleb@gmail.com",
+      ativo: true,
+      vinculo: "aluno",
+    }
+  ]
 }
